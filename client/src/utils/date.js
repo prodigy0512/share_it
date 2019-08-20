52ab6cd1bc6442f4c098c3c9633a87182354841f
nachiket <nachiketsdabhade@gmail.com>
2019-08-20 20:54:34 +0530

    added home page

--- client/src/utils/date.js
+++
@@ -1,9 +0,0 @@
-const formatDate = date => {
-    let month = date.getMonth()+1;
-    if(month < 10){
-        month = '0' + month;
-    }
-    return date.getDate()+'-'+ month +'-'+date.getFullYear();
-}
-
-export default formatDate;
